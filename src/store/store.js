ab106f22f6a19bade4d8df5bff5725b99542593b
Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2024-10-09 14:27:14 +0530

    Update store.js
    changed the file path for cartslice

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "../features/cart/cartSlice";
+import cartReducer from "../features/cart/CartSlice.js"
 import modalReducer from "../features/Modal/ModalSlice.js";
 
-// import counterReducer from "../features/cart/CounterSlice.jsx";
 
 export const store = configureStore({
   reducer: {
